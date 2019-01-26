20029cff99e1eb568de426b449574889af04f58a
TheAwesomeARay <andrewray4@gmail.com>
2019-01-25 22:58:23 -0600

    Adding tests. Failing to get validation to work

---
+++ src/Sum.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import PropTypes from 'prop-types';
+
+function Sum(props) {
+    return <h1>
+        {props.a} + {props.b} = {props.a + props.b}
+    </h1>;
+}
+Sum.propTypes = {
+    a: PropTypes.number.isRequired,
+    b: PropTypes.number.isRequired,
+}
+
+export default Sum;
--- src/index.js
+++ src/index.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import PropTypes from 'prop-types';
 
-let model = { clicks: 0 }
 
-function render() {
-    ReactDOM.render(<App 
-                        clicks={model.clicks} 
-                        onClick={() => { model.clicks++; render(); }}/>, document.getElementById('root'));
-}
-render();
+ReactDOM.render(<Greeting name={1} />, document.getElementById('root'));
 
+function Greeting(props) {
+    return <h1>Hello, {props.name}</h1>
+  }
+  
+  Greeting.propTypes = {
+    name: PropTypes.string
+  };
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
