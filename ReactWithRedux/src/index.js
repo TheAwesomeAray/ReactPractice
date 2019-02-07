dd7929ce1e6e481037291fe8717148ed3042ba1c
TheAwesomeARay <andrewray4@gmail.com>
2019-02-06 21:48:17 -0600

    App Architecture

--- ReactWithRedux/src/index.js
+++ ReactWithRedux/src/index.js
@@ -1,2 +1,23 @@
-/* eslint-disable no-console */
-console.log('hi');
+import 'babel-polyfill';
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, IndexRoute, browserHistory } from 'react-router';
+import './styles/styles.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import App from './components/App';
+import HomePage from './components/home/HomePage';
+import AboutPage from './components/about/AboutPage';
+import CouresPage from './components/course/CoursesPage';
+
+render (
+    <Router history={browserHistory}>
+        <Route path="/" component={App} >
+            <IndexRoute component={HomePage} />
+            <Route path="courses" component={CouresPage} /> 
+            <Route path="about" component={AboutPage} /> 
+        </Route>
+        
+        
+        </Router>, 
+    document.getElementById('app')
+);
